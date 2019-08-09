e3955e641db3a9ec708a8b2d9f62e66c237f5d3c
Melisa <melisaaltinsoy@gmail.com>
2019-08-09 19:26:47 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import "./App.css"
+
+const colors = ["#F2F5F1", "#D40920", "#1356A2", "#F7D842"]
 
 function App() {
+  const inputArray = Array(7).fill(0)
+
+  const coordinates = inputArray.map((n, i) => {
+    const randomX = Math.floor(Math.random() * (7 - 0) + 0) * 100
+    const randomY = Math.floor(Math.random() * (7 - 0) + 0) * 100
+    return [randomX, randomY]
+  })
+
+  console.log(coordinates)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <svg width="800" height="800" fill="none">
+      <rect
+        x="100"
+        y="100"
+        width="600"
+        height="600"
+        stroke="black"
+        strokeWidth="10"
+      />
+
+      {coordinates.map(i => {
+        return (
+          <rect
+            x="100"
+            y="100"
+            width={`${i[0]}`}
+            height={`${i[1]}`}
+            stroke="black"
+            strokeWidth="10"
+          />
+        )
+      })}
+    </svg>
+  )
 }
 
-export default App;
+export default App
